a5e8b6ea55b52f37903af089a3a2c2df0bc94cff
brandontjq <jinquan.tan.2014@sis.smu.edu.sg>
2017-07-20 23:19:02 +0800

    removed commented code

--- Desktop/testing/src/main/java/controller/MarketDataController.java
+++ Desktop/testing/src/main/java/controller/MarketDataController.java
@@ -112,29 +112,6 @@ public class MarketDataController extends Thread{
 					//testing algo2
 					algo2(ask3988, ask0386, ask0005, ask0388, ask0001);
 					
-					
-					
-					
-					/*if(i3988status.get("3988")== null){
-						//market buy
-						HashMap<String, Object> i3988Map = Instrument3988Controller.buy(Double.toString(askList.get(3)), "10");
-						i3988status.put("3988", i3988Map.get("3988"));
-					} else {
-						Instrument3988 i3988 = (Instrument3988) i3988status.get("3988");
-						// Bid >= 2% of what I bought
-						double myPrice = Double.parseDouble(i3988.getPrice());
-						if(bid >= 1.01 * Double.parseDouble(i3988.getPrice()) ){
-							Instrument3988Controller.sell("1", "10"); //market sell
-							i3988status.put("3988", null);
-							sell3988Counter = 0;
-						}
-						if(sell3988Counter == 6){
-							sell3988Counter = 0;
-							Instrument3988Controller.sell("1", "10"); //market sell
-							i3988status.put("3988", null);
-						}
-					}*/
-					
 				}
 				try {
 					Thread.sleep(6600);
@@ -153,96 +130,11 @@ public class MarketDataController extends Thread{
 				e.printStackTrace();
 
 			}
-
-			//return outputJSON;
-			//System.out.println(outputJSON);
-			//OrderController.getInstrumentDetails("0005");
-			
-			//Implement action
 			
 		}
 	}
 
-	/*public static void getMarketData() {
-		Thread thread = new Thread(new Runnable()
-		{
-			public void run()
-			{
-				// do some work here
-				System.out.println("Output from Server .... \n");
-				boolean brk = false;
-
-				while(brk == false){
-					String outputJSON = "";
-
-					try {
-
-						URL url = new URL("https://cis2017-exchange.herokuapp.com/api/market_data");
-						HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-						conn.setRequestMethod("GET");
-						conn.setRequestProperty("Accept", "application/json");
-
-						if (conn.getResponseCode() != 200) {
-							throw new RuntimeException("Failed : HTTP error code : "
-									+ conn.getResponseCode());
-						}
-
-						BufferedReader br = new BufferedReader(new InputStreamReader(
-								(conn.getInputStream())));
-
-						String output;
-
-						while ((output = br.readLine()) != null) {
-							outputJSON += output;
-							ArrayList<String> test = JsonPath.read(outputJSON, "$[*].symbol");
-
-
-						}
-
-						conn.disconnect();
-
-					} catch (MalformedURLException e) {
-
-						e.printStackTrace();
-
-					} catch (IOException e) {
-
-						e.printStackTrace();
-
-					}
-
-					//return outputJSON;
-					System.out.println(outputJSON);
-
-
-				}
-			}
-		});
-		//thread.start();
-
-
-
-		//Below code to test out multi-threading
-
-		//OrderController.getInstrumentDetails("0001");
-		//OrderController t1 = new OrderController();
-		//t1.getInstrumentDetails("0005");
-		try {
-			thread.join();
-			t1.join();
-
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-
-
-
-
-
-
-	}*/
+	
 	
 	public static void algo1(double askPrice){
 		boolean bought = false;
